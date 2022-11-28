11318cc7bcba28322c891cc689f67e5ed34993b0
urstrulycharan1 <118816413+urstrulycharan1@users.noreply.github.com>
2022-11-28 18:07:27 +0530

    Add files via upload

---
+++ p21.py
@@ -0,0 +1,9 @@
+pin=int(input("enter the pin number"))
+sum=1004
+if pin==sum:
+    print("continue")
+    print("transaction successful")
+else:
+    print("not valid")
+    print("incorrec pin number")
+    print("Please try again")
